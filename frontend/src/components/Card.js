d8b2ea30d10257cbe2f37a08cfad8515e1e4f064
Михаил Мякинин <linkin-park_94@mail.ru>
2023-09-17 03:26:51 +0300

    frontend + backend

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Card = ({ card, onCardClick, onCardLike, onCardDelete }) => {
 
   const currentUser = useContext(CurrentUserContext);
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   const isLiked = card.likes.some(i => i === currentUser._id);
   const cardLikeButtonClassName = `element__like ${isLiked && 'element__like_active'}`;
 
